a8c58bb111181a151662bc9da8ad015dcea5a26a
Vesa Laakso <laakso.vesa@gmail.com>
2013-01-01 20:09:44 +0200

    Mapping and tilesetting and stuff.

---
+++ data/Special tiles.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Special tiles" tilewidth="32" tileheight="32" spacing="1" margin="1">
+ <image source="tileset_meta.png" width="133" height="67"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" value="all"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision" value="top-left"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision" value="bottom-left"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collision" value="bottom-right"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collision" value="top-right"/>
+  </properties>
+ </tile>
+</tileset>
