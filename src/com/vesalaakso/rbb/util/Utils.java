2142f4db75a71b3c0c55d78ea070a313e9c483c4
Vesa Laakso <laakso.vesa@gmail.com>
2013-01-07 23:55:57 +0200

    Utils to calculate the screen->world coordinate change

--- src/com/vesalaakso/rbb/util/Utils.java
+++ src/com/vesalaakso/rbb/util/Utils.java
@@ -2,6 +2,9 @@ package com.vesalaakso.rbb.util;
 
 import org.newdawn.fizzy.Vector;
 
+import com.vesalaakso.rbb.RubberBandBall;
+import com.vesalaakso.rbb.model.Camera;
+
 /**
  * Some static methods to aid in various little things.
  * 
@@ -34,6 +37,42 @@ public class Utils {
 
 		// Return the curved endpoint
 		return new Vector(x, y);
+	}
+
+	/**
+	 * Calculate the current world x-coordinate for the given screen
+	 * x-coordinate.
+	 * 
+	 * @param screenX
+	 *            the screen x-coordinate to transform
+	 * @return world x-coordinate corresponding to the given screen x-coordinate
+	 */
+	public static float screenToWorldX(float screenX) {
+		// Need to calc half of the width of the screen for this
+		float screenHalfWidth = RubberBandBall.SCREEN_WIDTH * 0.5f;
+
+		// Calculate the screen coordinate
+		float x = Camera.get().getX() + screenX - screenHalfWidth;
+
+		return x;
+	}
+
+	/**
+	 * Calculate the current world y-coordinate for the given screen
+	 * y-coordinate.
+	 * 
+	 * @param screenY
+	 *            the screen y-coordinate to transform
+	 * @return world y-coordinate corresponding to the given screen y-coordinate
+	 */
+	public static float screenToWorldY(float screenY) {
+		// Need to calc half of the height of the screen for this
+		float screenHalfHeight = RubberBandBall.SCREEN_HEIGHT * 0.5f;
+
+		// Calculate the screen coordinate
+		float y = Camera.get().getY() + screenY - screenHalfHeight;
+
+		return y;
 	}
 
 }
