8f753d12e12d6d6f46570d43d5c968633ca71f06
Vesa Laakso <laakso.vesa@gmail.com>
2013-01-20 20:38:25 +0200

    Another try to fix tilemaps but no

--- src/com/vesalaakso/rbb/data/levels/Tiles.tsx
+++ src/com/vesalaakso/rbb/data/levels/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Normal tiles" tilewidth="32" tileheight="32" spacing="2" margin="1">
+<tileset name="Normal tiles" tilewidth="32" tileheight="32" spacing="0" margin="0">
  <image source="tileset.png" width="133" height="364"/>
 </tileset>
